4b45af410455e1b5c43fdb40b20ed6a3cc89a710
samuelport12 <portugal78samuel@gmail.com>
2025-03-31 15:11:35 -0300

    Update main.py

--- projeto23/main.py
+++ projeto23/main.py
@@ -3,30 +3,28 @@ import pyautogui as pt
 import os
 import time 
 
-data_venc = str(input("digite a data de venc: "))
-fornecedor = str(input("Fornecedor: ")
+data_venc = str(input("Digite a data de venc: "))
+fornecedor = str(input("Fornecedor: "))
 
-pt.FAILSAFE = True #MOVER PARA CANTO SUPERIOR ESQ.
-df = pd.read_excel("projeto23\Planilha.xlsx")
+pt.FAILSAFE = True  # Mover para canto superior esquerdo
+df = pd.read_excel("projeto23/Planilha.xlsx")  # Corrigi a barra invertida
+
 # Conta o número de linhas
 num_linhas = df.shape[0]
 
-os.system('start notepad') #apenas para testes
+os.system('start notepad')  # Apenas para testes
 time.sleep(0.1)
 
 i = 0
-while i <= num_linhas:
-    valorID = df.loc[a,"ID"] #LOC[+LINHA,COLUNA]
-    
-    valorVAL = df.loc[a,"VALOR"]
-    
-    valorDATA = df.loc[a,"Emissão"]
+while i < num_linhas:  # Alterado de `<=` para `<`
+    valorID = df.loc[i, "ID"]
+    valorVAL = df.loc[i, "VALOR"]
+    valorDATA = df.loc[i, "Emissão"]
+    valorPROP = df.loc[i, "PROP"]
 
-    valorPROP = df.loc[a,"PROP"] 
-
-    #primeira_parte_do_lançamento
-    pt.PAUSE = 0.2   
-    pt.write(f'{valorID}')  # Digita o ID
+    # Primeira parte do lançamento
+    pt.PAUSE = 0.2
+    pt.write(f'{valorID}')
     pt.press('tab')
 
     pt.write('1')
@@ -39,20 +37,19 @@ while i <= num_linhas:
     pt.press('tab')
     pt.press('tab')
 
-    pt.write(data_venc) # Vencimento - data unica
+    pt.write(data_venc)  # Vencimento - data única
     pt.press('tab')
 
-    # # Pressiona seta para baixo 5 vezes
     for _ in range(5):
-         pt.press('down')
+        pt.press('down')
     
     pt.press('tab')
     pt.press('tab')
-    pt.press('down')  # 1 seta para baixo
+    pt.press('down')
     pt.press('tab')
     
     for _ in range(2):
-         pt.press('down')
+        pt.press('down')
     pt.press('tab')
 
     pt.write(f'{valorVAL}')
@@ -60,7 +57,7 @@ while i <= num_linhas:
     pt.press('tab')
     pt.press('tab')
 
-#salgueiro - FILIAL CE 
+    # Salgueiro - Filial CE
     if fornecedor == "221":
         if valorPROP == "TRUE":
             apropriacao = "8001"
@@ -68,7 +65,7 @@ while i <= num_linhas:
         else:
             apropriacao = "8000"
             conta = "501"
-#Serra talhada - MATRIZ 
+    # Serra Talhada - Matriz
     elif fornecedor == "38":
         if valorPROP == "TRUE":
             apropriacao = "5222"
@@ -76,8 +73,8 @@ while i <= num_linhas:
         else:
             apropriacao = "5036"
             conta = "501"
-#arcoverde - FILIAL PE 
-     elif fornecedor == "10750":
+    # Arcoverde - Filial PE
+    elif fornecedor == "10750":  
         if valorPROP == "TRUE":
             apropriacao = "5222"
             conta = "101"
@@ -96,26 +93,23 @@ while i <= num_linhas:
 
     pt.press('enter')
 
-    time.sleep(1.5) #esperar para não criar problema com aceleração
-    #segunda_parte_do_lançamento 
+    time.sleep(1.5)
+
+    # Segunda parte do lançamento
     for _ in range(5):
-    pt.press('tab')
+        pt.press('tab')
 
     time.sleep(1.5)
     
     pt.press('tab')
-    
     pt.press('enter')
     
     for _ in range(7):
         pt.press('tab')
     
     pt.press('enter')
-    
-    # Aguarda 2 segundos
-    time.sleep(2) #esperar para não criar problema com aceleração
 
-    #término_do_lançamento - nova linha será processada
+    time.sleep(2)  # Esperar para não criar problema com aceleração
 
+    # Nova linha será processada
     i += 1
-
