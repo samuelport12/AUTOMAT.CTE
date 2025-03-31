cb8595c08f8627c8e1115295fdc256531bd5f8f9
samuelport12 <portugal78samuel@gmail.com>
2025-03-31 15:04:00 -0300

    Update main.py
    Segunda inserção

--- projeto23/main.py
+++ projeto23/main.py
@@ -3,19 +3,28 @@ import pyautogui as pt
 import os
 import time 
 
+data_venc = str(input("digite a data de venc: "))
+fornecedor = str(input("Fornecedor: ")
+
+pt.FAILSAFE = True #MOVER PARA CANTO SUPERIOR ESQ.
 df = pd.read_excel("projeto23\Planilha.xlsx")
-#print(df.head(1))
-os.system('start notepad')
+# Conta o número de linhas
+num_linhas = df.shape[0]
+
+os.system('start notepad') #apenas para testes
 time.sleep(0.1)
 
-a=0
-while a < 5:
+i = 0
+while i <= num_linhas:
     valorID = df.loc[a,"ID"] #LOC[+LINHA,COLUNA]
     
     valorVAL = df.loc[a,"VALOR"]
     
-    valorDATA = df.loc[a,"DATA"]
+    valorDATA = df.loc[a,"Emissão"]
 
+    valorPROP = df.loc[a,"PROP"] 
+
+    #primeira_parte_do_lançamento
     pt.PAUSE = 0.2   
     pt.write(f'{valorID}')  # Digita o ID
     pt.press('tab')
@@ -30,7 +39,7 @@ while a < 5:
     pt.press('tab')
     pt.press('tab')
 
-    pt.write('21/04/2025') # Vencimento - data unica
+    pt.write(data_venc) # Vencimento - data unica
     pt.press('tab')
 
     # # Pressiona seta para baixo 5 vezes
@@ -51,10 +60,35 @@ while a < 5:
     pt.press('tab')
     pt.press('tab')
 
-    pt.write("8000")
+#salgueiro - FILIAL CE 
+    if fornecedor == "221":
+        if valorPROP == "TRUE":
+            apropriacao = "8001"
+            conta = "101"
+        else:
+            apropriacao = "8000"
+            conta = "501"
+#Serra talhada - MATRIZ 
+    elif fornecedor == "38":
+        if valorPROP == "TRUE":
+            apropriacao = "5222"
+            conta = "101"
+        else:
+            apropriacao = "5036"
+            conta = "501"
+#arcoverde - FILIAL PE 
+     elif fornecedor == "10750":
+        if valorPROP == "TRUE":
+            apropriacao = "5222"
+            conta = "101"
+        else:
+            apropriacao = "5036"
+            conta = "501"
+    
+    pt.write(apropriacao)
     pt.press('tab')
 
-    pt.write('501')
+    pt.write(conta)
     pt.press('tab')
 
     pt.write('.')
@@ -62,5 +96,26 @@ while a < 5:
 
     pt.press('enter')
 
-    a += 1
+    time.sleep(1.5) #esperar para não criar problema com aceleração
+    #segunda_parte_do_lançamento 
+    for _ in range(5):
+    pt.press('tab')
+
+    time.sleep(1.5)
+    
+    pt.press('tab')
+    
+    pt.press('enter')
+    
+    for _ in range(7):
+        pt.press('tab')
+    
+    pt.press('enter')
+    
+    # Aguarda 2 segundos
+    time.sleep(2) #esperar para não criar problema com aceleração
+
+    #término_do_lançamento - nova linha será processada
+
+    i += 1
 
